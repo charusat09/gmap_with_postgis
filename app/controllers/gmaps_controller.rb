781cd833dbaab97f391fb2c9c19588b60fe9d1e6
Mayur Patel <mayurt20@gmail.com>
2016-05-21 15:25:07 +0530

    adds update feature

--- app/controllers/gmaps_controller.rb
+++ app/controllers/gmaps_controller.rb
@@ -13,6 +13,7 @@ class GmapsController < ApplicationController
   	puts query
   	polygon_obj = RewardLocation.where(query).first
   	if polygon_obj.present?
+      @polygon_obj_name = polygon_obj.name 
   		polygon = polygon_obj.polygon 
 			polygon_coordinates = RGeo::GeoJSON.encode(polygon)["coordinates"][0]
 	  	hash = []
@@ -46,8 +47,6 @@ class GmapsController < ApplicationController
   	query_string += "))"
 
     polygon = RewardLocation.create(polygon: query_string, name: params[:name])
-
-
   	respond_to do |format|
       if polygon
         format.js
@@ -56,6 +55,28 @@ class GmapsController < ApplicationController
   end
 
   def save_updated_polygon
-    puts "got it"
+    polygon = RewardLocation.where(name: params[:name]).first
+
+    polygon_shape = params[:polygon]
+
+    query_string = "POLYGON(("
+    (0..(polygon_shape.length-1)).each do |coordinate|
+      query_string += polygon_shape["#{coordinate}"][0]
+      query_string += " "
+      query_string += polygon_shape["#{coordinate}"][1]
+      query_string += ","
+    end
+    query_string.chomp!(",")
+    query_string += "))"
+    saved_polygon = RewardLocation.where(name: params[:name]).first
+    new_polygon = RewardLocation.update(polygon: query_string, name: params[:name])
+
+
+    respond_to do |format|
+      if new_polygon
+        format.js
+      end
+    end
+
   end
 end
